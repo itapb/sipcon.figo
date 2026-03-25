c1f51e23dcefc528d60790ca786fa205c72bcc25
Sarahy Chirinos <132629977+sarahybcg@users.noreply.github.com>
2026-03-25 13:06:53 -0400

    integracion con figo (#3)
    Co-authored-by: ad-apb023 <ad-apb023@APB-C211>

---
+++ Data/dTransaction.cs
@@ -0,0 +1,167 @@
+﻿using System.Data;
+using Models;
+using Util;
+
+namespace Data
+{
+    public class dTransaction
+    {
+        private readonly SemaphoreSlim _semaphore;
+
+        public dTransaction()
+        {
+            Util.Setting.GetSettings(true);
+            _semaphore = new SemaphoreSlim(100, 150);
+        }
+
+        #region RECEPCION DE REPUESTOS
+        public async Task<Response<Models.Result>> PostReceptionParts(List<Models.ReceptionParts> _list)
+        {
+            await _semaphore.WaitAsync(Util.Setting.TimeOut);
+            try
+            {
+                return await _PostReceptionParts(_list);
+            }
+            finally
+            {
+                _semaphore.Release();
+            }
+        }
+
+        private async Task<Response<Models.Result>> _PostReceptionParts(List<Models.ReceptionParts> _list)
+        {
+            Response<Models.Result> _response = new Response<Models.Result>();
+            try
+            {
+                string _jsonstring = Util.Json.ConvertToJsonString(_list);
+                Parameter _parameter = new Parameter();
+                _parameter.AddSqlParameter("@DATA", _jsonstring);
+                Mapping _mapping = new Mapping();
+                _mapping.SetDefaultPostMapping();
+                Util.Data _data = Util.Data.GetInstance();
+                DataTable _table = await _data.GetDataTable("USP_POST_RECEPTIONPARTS_FIGO", _parameter);
+                _response.Data = _data.GetItem<Models.Result>(_mapping, _table);
+                _response.SetPostResponse();
+            }
+            catch (Exception ex)
+            {
+                _response.SetError(ex);
+            }
+            return _response;
+        }
+        #endregion
+
+        #region DESPACHOS POR FACTURAR
+        public async Task<Response<List<Models.DispatchsToInvoincing>>> GetDispatchsToInvoicing()
+        {
+            await _semaphore.WaitAsync(Util.Setting.TimeOut);
+            try
+            {
+                return await _GetDispatchsToInvoicing();
+            }
+            finally
+            {
+                _semaphore.Release();
+            }
+        }
+
+        private async Task<Response<List<Models.DispatchsToInvoincing>>> _GetDispatchsToInvoicing()
+        {
+            Response<List<Models.DispatchsToInvoincing>> _response = new Response<List<Models.DispatchsToInvoincing>>();
+            try
+            {
+                Mapping _mapping = new Mapping();
+                _mapping.AddItem("Reference", "IDCONTROL");
+                _mapping.AddItem("SupplierVat", "VVATSUPPLIER");
+                _mapping.AddItem("DealerVat", "VVATDEALER");
+                _mapping.AddItem("InnerCode", "VINNERCODE");
+                _mapping.AddItem("Quantity", "IDISPATCHED");
+
+                Util.Data _data = Util.Data.GetInstance();
+                DataTable _table = await _data.GetDataTable("USP_GET_DISPATCHSTOINVOINCING_FIGO", null);
+                _response.Data = _data.GetList<Models.DispatchsToInvoincing>(_mapping, _table);
+                _response.SetGetResponse(_table);
+            }
+            catch (Exception ex)
+            {
+                _response.SetError(ex);
+            }
+            return _response;
+        }
+        #endregion
+
+        #region DESPACHOS FACTURADOS
+        public async Task<Response<Models.Result>> PostInvoicedDispatches(List<Models.InvoicedDispatches> _list)
+        {
+            await _semaphore.WaitAsync(Util.Setting.TimeOut);
+            try
+            {
+                return await _PostInvoicedDispatches(_list);
+            }
+            finally
+            {
+                _semaphore.Release();
+            }
+        }
+
+        private async Task<Response<Models.Result>> _PostInvoicedDispatches(List<Models.InvoicedDispatches> _list)
+        {
+            Response<Models.Result> _response = new Response<Models.Result>();
+            try
+            {
+                string _jsonstring = Util.Json.ConvertToJsonString(_list);
+                Parameter _parameter = new Parameter();
+                _parameter.AddSqlParameter("@DATA", _jsonstring);
+                Mapping _mapping = new Mapping();
+                _mapping.SetDefaultPostMapping();
+                Util.Data _data = Util.Data.GetInstance();
+                DataTable _table = await _data.GetDataTable("USP_POST_INVOICEDDISPATCHES_FIGO", _parameter);
+                _response.Data = _data.GetItem<Models.Result>(_mapping, _table);
+                _response.SetPostResponse();
+            }
+            catch (Exception ex)
+            {
+                _response.SetError(ex);
+            }
+            return _response;
+        }
+        #endregion
+
+        #region PLACAS POR ASIGNAR
+        public async Task<Response<List<Models.PlatesToAssign>>> GetPlatesToAssign()
+        {
+            await _semaphore.WaitAsync(Util.Setting.TimeOut);
+            try
+            {
+                return await _GetPlatesToAssign();
+            }
+            finally
+            {
+                _semaphore.Release();
+            }
+        }
+
+        private async Task<Response<List<Models.PlatesToAssign>>> _GetPlatesToAssign()
+        {
+            Response<List<Models.PlatesToAssign>> _response = new Response<List<Models.PlatesToAssign>>();
+            try
+            {
+                Mapping _mapping = new Mapping();
+                _mapping.AddItem("Plate", "VPLATE");
+                _mapping.AddItem("SupplierVat", "VVATSUPPLIER");
+                _mapping.AddItem("Vin", "VVIN"); 
+
+                Util.Data _data = Util.Data.GetInstance();
+                DataTable _table = await _data.GetDataTable("USP_GET_PLATESTOASSIGN_FIGO", null);
+                _response.Data = _data.GetList<Models.PlatesToAssign>(_mapping, _table);
+                _response.SetGetResponse(_table);
+            }
+            catch (Exception ex)
+            {
+                _response.SetError(ex);
+            }
+            return _response;
+        }
+        #endregion
+    }
+}
---
+++ Models/DispatchsToInvoincing.cs
@@ -0,0 +1,17 @@
+﻿using Swashbuckle.AspNetCore.Annotations;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Models
+{
+    public class DispatchsToInvoincing
+    {
+        [SwaggerIgnore] public string? Reference { get; set; } = string.Empty;
+        [SwaggerIgnore] public string? SupplierVat { get; set; } = string.Empty;
+        [SwaggerIgnore] public string? DealerVat { get; set; } = string.Empty;
+        [SwaggerIgnore] public string? InnerCode { get; set; } = string.Empty;
+        [SwaggerIgnore] public int? Quantity { get; set; }
+    }
+}
