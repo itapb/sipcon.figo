22b66c3f02603faf15f5d9026ed48cef3e68a90d
JuanApbGroup21 <juanguarecuco@apbgroup.net>
2026-03-25 12:21:55 -0400

    Figomodels jd (#2)
    * creacion de metodos para contacto y modelos
    
    * key de modelo
    
    * rangos de los atributos
    
    * sp de maestros
    
    * creacion de models

--- Models/Model.cs
+++ Models/Model.cs
@@ -8,19 +8,18 @@ using Swashbuckle.AspNetCore.Annotations;
 
 namespace Models
 {
-    public class Model:Record   
+    public class Model  
 
     {
-        
 
-        [Required]
+
+        [Required(ErrorMessage = "El Description es obligatorio")] [MinLength(1, ErrorMessage = "Mínimo 1 caracteres")] [MaxLength(100, ErrorMessage = "Máximo 100 caracteres")]
         public string? Description { get; set; }
 
-        [Required]
-        public string? Name { get; set; }
+        [Required(ErrorMessage = "El Description es obligatorio")] [MinLength(1, ErrorMessage = "Mínimo 1 caracteres")] [MaxLength(20, ErrorMessage = "Máximo 20 caracteres")]
+         public string? Name { get; set; }
 
-   
-        [SwaggerIgnore]
+        [Required(ErrorMessage = "El SupplierVat es obligatorio")][MinLength(1, ErrorMessage = "Mínimo 1 caracteres")][MaxLength(12, ErrorMessage = "Máximo 12 caracteres")]
         public String? SupplierVat { get; set; }
 
 
