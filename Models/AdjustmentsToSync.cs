afff5683da813eec80955c2122433044cf43f159
Sarahy Chirinos <132629977+sarahybcg@users.noreply.github.com>
2026-03-26 09:58:57 -0400

    Adjustment (#4)
    * integracion con figo
    
    * ADJUSTMENT
    
    ---------
    
    Co-authored-by: ad-apb023 <ad-apb023@APB-C211>

---
+++ Models/AdjustmentsToSync.cs
@@ -0,0 +1,20 @@
+﻿using Swashbuckle.AspNetCore.Annotations;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Models
+{
+    public class AdjustmentsToSync
+    {
+        [SwaggerIgnore] public int? AdjustmentNumber { get; set; } 
+        [SwaggerIgnore] public string? AdjustmentDate { get; set; } = string.Empty;
+        [SwaggerIgnore] public string? SupplierVat { get; set; } = string.Empty;
+        [SwaggerIgnore] public string? Observation { get; set; } = string.Empty;
+        [SwaggerIgnore] public string? InnerCode { get; set; } = string.Empty;
+        [SwaggerIgnore] public int? Quantity { get; set; }
+        [SwaggerIgnore] public string? Type { get; set; } = string.Empty;
+        [SwaggerIgnore] public string? Concept { get; set; } = string.Empty;
+    }
+}
---
+++ Models/SyncAdjustment.cs
@@ -0,0 +1,13 @@
+﻿using Swashbuckle.AspNetCore.Annotations;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Models
+{
+    public class SyncAdjustment
+    {
+        [Required] public int? AdjustmentNumber { get; set; }
+    }
+}
