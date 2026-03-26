430fc27911c5e238d698c3ba86b1e51026327067
ad-apb023 <ad-apb023@APB-C211>
2026-03-26 09:27:54 -0400

    ADJUSTMENT

---
+++ Models/AdjustmentsDetailsToSync.cs
@@ -0,0 +1,15 @@
+﻿using Swashbuckle.AspNetCore.Annotations;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Models
+{
+    public class AdjustmentsDetailsToSync : Details
+    {
+        [Required] public string? Type { get; set; } = string.Empty;
+        [Required] public string? Concept { get; set; } = string.Empty;
+    }
+}
---
+++ Models/AdjustmentsToSyncWithContext.cs
@@ -0,0 +1,17 @@
+﻿using Swashbuckle.AspNetCore.Annotations;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Models
+{
+    public class AdjustmentsToSyncWithContext
+    {
+        [SwaggerIgnore] public int? AdjustmentNumber { get; set; }
+        [SwaggerIgnore] public string? AdjustmentDate { get; set; } = string.Empty;
+        [SwaggerIgnore] public string? SupplierVat { get; set; } = string.Empty;
+        [SwaggerIgnore] public string? Observation { get; set; } = string.Empty;
+        [SwaggerIgnore] public List<AdjustmentsDetailsToSync> Detail { get; set; } = new();
+    }
+}
