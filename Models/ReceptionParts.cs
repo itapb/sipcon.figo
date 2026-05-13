fad80419341ec5e31c8b56316e3eb7320708f963
JuanApbGroup21 <juanguarecuco@apbgroup.net>
2026-05-13 10:27:23 -0400

    Fixreceptionparts jd (#13)
    * creacion de metodos para contacto y modelos
    
    * key de modelo
    
    * rangos de los atributos
    
    * sp de maestros
    
    * creacion de models
    
    * CMABIOS
    
    * creacion de asignacion de placas, y despacho de vehiculos
    
    * CAMBIOS EN ASIGNACON DE VEHICULOS
    
    * correcion
    
    * PostaccountReceivable
    
    * correcion de contact, y asignacion de vehiculos por planta
    
    * correccion de endpoint de vehiculos-figo
    
    * cambio de nombre vehiculo
    
    * cambio de contacto
    
    * creacion de endpoint de consolidacion de pagos
    
    * creacion de endpoint de recepcion
    
    * sincronizacion con figo
    
    * PostRate
    
    * correcion sp
    
    * correcion de parametro
    
    * correciones
    
    * provider opcional

--- Models/ReceptionParts.cs
+++ Models/ReceptionParts.cs
@@ -11,7 +11,7 @@ namespace Models
         [Required(ErrorMessage = "Fecha de recepción de los repuestos es obligatoria")] public string? ReceptionDate { get; set; } = string.Empty;
         [Required(ErrorMessage = "Número de documento de recepción es obligatorio")][MaxLength(10, ErrorMessage = "Máximo 10 caracteres")] public string? ReceptionNumber { get; set; } = string.Empty;
         [Required(ErrorMessage = "Rif de la planta es obligatorio")][MaxLength(12, ErrorMessage = "Máximo 12 caracteres")] public string? SupplierVat { get; set; } = string.Empty;
-        [Required(ErrorMessage = "Rif del proveedor es obligatorio")][MaxLength(12, ErrorMessage = "Máximo 12 caracteres")] public string? ProviderVat { get; set; } = string.Empty;
+        public string? ProviderVat { get; set; } = string.Empty;
         [Required] public List<Details> Detail { get; set; } = new();
     }
 }
